d63c7303708af7c323a9b33d3e84326cd1300b68
freeway704 <freeway704@gmail.com>
2014-12-20 12:11:38 +0800

    跑酷場景+自動向前移動+跳躍

---
+++ Assets/Scripts/player.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+var speed : float = 5.0f;
+var jumpForce : float = 800.0f;
+
+function Start () {
+
+}
+
+function Update () {
+	rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+	if(Input.GetButtonDown("Jump")){
+		rigidbody2D.AddForce(new Vector2(0,jumpForce));
+	}
+}
