6265aca1b889a9fb3c289adea5cf89da93e35027
freeway704 <freeway704@gmail.com>
2014-12-20 15:00:46 +0800

    跑酷版本 防止連跳 建立死亡點 建立重生點

---
+++ Assets/Scripts/cameraFollower.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var target : Transform;
+var leftoffset: float = 19.0f;
+var distance = -10;
+var damping = 10;
+var lockRotation : boolean = true;
+
+function Update () {
+	var wantedPosition = target.TransformPoint(leftoffset, 0, -distance);
+	transform.position = Vector3.Lerp (transform.position, new Vector3(wantedPosition.x, 0, transform.position.z), Time.deltaTime * damping);
+
+	transform.LookAt (target, target.up);
+	
+	if(lockRotation) {
+		transform.localRotation = Quaternion.identity;
+	}
+}
